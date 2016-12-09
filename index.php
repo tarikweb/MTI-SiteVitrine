b336197de751de24d0b3c468255c2516f1127510
Durosinmi Lamine <durmalick@MacBook-Air-de-Durosinmi.local>
2016-12-09 17:02:14 +0100

    mise a jour d'index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 <body>
 <?php>
-$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 <?>
 <div id="slide1">
 
@@ -29,14 +29,13 @@ $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
 
 <h2>Bienvenue sur le site officiel <br/>de la république des lamas</h2>
 
-<p>Lorem Salu bissame ! Wie geht's les samis ? Hans apporte moi une Wurschtsalad avec un picon bitte, s'il te plaît.Voss ? Une Carola et du Melfor ? Yo dû, espèce de Knäckes, ch'ai dit un picon !<br/><br/>
+<p>
+<?php> 
+$texte = $bdd->query('SELECT text_contenu FROM site');
+echo $texte;
 
-Hopla vous savez que la mamsell Huguette, la miss Miss Dahlias du messti de Bischheim était au Christkindelsmärik en compagnie de Richard Schirmeck (celui qui a un blottkopf), le mari de Chulia Roberstau, qui lui trempait sa Nüdle dans sa Schneck ! Yo dû, Pfourtz ! Ch'espère qu'ils avaient du Kabinetpapier, Gal !!<br/><br/>
-
-Yoo ch'ai lu dans les DNA que le Racing a encore perdu contre Oberschaeffolsheim. Verdammi et moi ch'avais donc parié deux knacks et une flammekueche. Ah so ? T'inquiète, ch'ai ramené du schpeck, du chambon, un kuglopf et du schnaps dans mon rucksack. Allez, s'guelt ! Wotch a kofee avec ton bibalaekaess et ta wurscht ? Yeuh non che suis au réchime, je ne mange plus que des Grumbeere light et che fais de la chym avec Chulien. Tiens, un rottznoz sur le comptoir.!<br/><br/>
-
-Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci vielmols mais che dois partir à la Coopé de Truchtersheim acheter des mänele et des rossbolla pour les gamins. Hopla tchao bissame ! Consectetur adipiscing elit</p>
-
+<?>
+</p>
 </div>   	
 
 </div> 
