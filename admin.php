715206d238d29d19a222e0669631f2ae03279b3a
tarikweb <tarik.abbas94@gmail.com>
2016-12-09 16:49:20 +0100

    majde admin.php

---
+++ admin.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Administration</title>
+
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<header>
+</header>
+<section>
+ <form>
+<label>Titre dans le navigateur</label>
+<input name="titre"></input>
+<label>Titre 1</label>
+<input name="h1"></input>
+
+<label>Titre 2</label>
+<input name="h2"></input>
+
+<label>Adresse</label>
+<input name="txt_adresse"></input>
+
+<label>Contenu</label>
+<input name="text_contenu"></input>
+
+<label>Image</label>
+<input name="img_slide1"></input>
+
+<label>couleur slide 2</label>
+<input name="color_slide2"></input>
+
+<label>couleur slide 3</label>
+<input name="color_slide3"></input>
+
+<button type="submit"></button>
+</form>
+
+</section>
+</body>
